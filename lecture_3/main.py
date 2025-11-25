74f5d8bf9fa972cbb5b1edb3342f1d5881fb58b7
dmitry-poznyak <dimapoznyak1227@gmail.com>
2025-11-25 22:22:43 +0300

    start

---
+++ lecture_3/main.py
@@ -0,0 +1,11 @@
+students = []
+
+while True:
+    print("\n--- Student Grade Analyzer ---")
+    print("1. Add a new student")
+    print("2. Add grades for a student")
+    print("3. Show report")
+    print("4. Find top performer")
+    print("5. Exit")
+
+    choice = input("Enter choice (1-5): ")
